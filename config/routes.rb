b5d7f8012350a269745089b4a23a2ea6c80ede91
benjibennn <benjaminwong94@hotmail.my>
2018-05-16 18:00:20 +0800

    Added view teacher function for admin Added create teacher function for admin Added create student under teacher for admin Added view client for admin Added create client for admin Added view student for teacher Added delete student for teacher Added create student for teacher Added create client for teacher

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+	resources :students
+  	resources :users, controller: "users", only: [:create, :edit, :update, :destroy] do
+	  	 member do 
+	        get 'create_teacher'
+	        get 'list_client'
+	    end
+  	end
 
-  resources :teachers
-  resources :users, controller: "users", only: [:create, :edit, :update, :destroy] do
-  	 member do 
-        get 'list_teacher'
-        get 'create_teacher'
-        get 'list_client'
-    end
-  end
-
-
+  get 'user/list_teacher' => 'users#list_teacher'
+  get 'student/:id/new' => 'students#new'
+  get 'user/list_student' => 'users#list_student'
   root 'users#index'
 
 end
