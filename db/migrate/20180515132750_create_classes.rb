4b861c4aa7f5f573050753a5f97856fcdb9d5bd6
puiyoke <puiyoke8888@hotmail.com>
2018-05-22 16:09:42 +0800

    migration for classes readded

---
+++ db/migrate/20180515132750_create_classes.rb
@@ -0,0 +1,9 @@
+class CreateClasses < ActiveRecord::Migration[5.1]
+    def change
+      create_table :classes do |t|
+        t.timestamps null: false
+        t.references :user, foreign_key: true
+        t.references :student, foreign_key: true
+      end
+    end
+  end
